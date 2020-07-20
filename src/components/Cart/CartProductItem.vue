c38103dfb0675e6d05589d8da0a7975e310f596c
andrebizeul <andybiz7401@gmail.com>
2020-07-20 10:52:49 +0200

    first commit

---
+++ src/components/Cart/CartProductItem.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="d-flex flex-row justify-content-between p-2">
+    <span style="width:130px;"> {{ item.title }} </span>
+    <span><strong> {{ item.price }} € </strong></span>
+  </div>
+</template>
+
+<script>
+
+export default {  
+  props:['item']
+  }
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Cart/CartProductsList.vue
@@ -0,0 +1,25 @@
+<template>
+   <div class="d-flex flex-column p-5">
+     <h5 class="border-bottom p-5">Mon panier</h5>
+      <cart-product-item  v-for="item in cart"  :key="item.id"  :item="item"> </cart-product-item>
+      <!-- <hr class = 'w-100'> -->
+   </div>
+</template>
+
+<script>
+
+  import CartProductItem from './CartProductItem.vue';
+
+
+export default { 
+  components: {
+    CartProductItem
+  } ,
+  props:['cart']
+  }
+
+</script>
+
+<style>
+
+</style>
